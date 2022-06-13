efe2ba09afd812a40899d163fd497293d2a1f6c2
Mrzhao <1792335584@qq.com>
2022-06-13 23:58:31 +0800

    master commit2 use idea

--- src/main/java/com/zyh/git/GitTest.java
+++ src/main/java/com/zyh/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Hello Git222");
         System.out.println("Hello Git3333");
         System.out.println("Hello Git4");
+        System.out.println("master1");
     }
 }
